6bcde8108fcb589eeb0add77214d5f5c15f23a75
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-11-29 13:39:34 +0500

    codeview

---
+++ app/src/main/java/com/example/test_navigation/domain/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.test_navigation.domain.utils
+
+class Constants {
+    companion object{
+        const val DEG_ROTATE = 45
+        const val HALF = 2
+        const val TIME_UPDATE = 1000L
+        const val DISTANCE_UPDATE = 1f
+    }
+}
--- app/src/main/java/com/example/test_navigation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/test_navigation/ui/home/HomeFragment.kt
@@ -1,14 +1,10 @@
 package com.example.test_navigation.ui.home
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
-import android.hardware.Sensor
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -18,14 +14,17 @@ import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.test_navigation.R
 import com.example.test_navigation.databinding.FragmentHomeBinding
+import com.example.test_navigation.domain.utils.Constants.Companion.DEG_ROTATE
+import com.example.test_navigation.domain.utils.Constants.Companion.DISTANCE_UPDATE
+import com.example.test_navigation.domain.utils.Constants.Companion.HALF
+import com.example.test_navigation.domain.utils.Constants.Companion.TIME_UPDATE
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -39,15 +38,33 @@ import com.google.android.gms.maps.model.MarkerOptions
 class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
-    private lateinit var googleMap: GoogleMap
+    private var googleMap: GoogleMap? = null
     private lateinit var binding: FragmentHomeBinding
     private lateinit var locationManager: LocationManager
     private lateinit var userMarker: Marker
     private lateinit var customMarkerOptions: MarkerOptions
     private var lastLocation: Location? = null
     private var speed: Double = 0.0
+    private var dialog: AlertDialog? = null
 
-    @SuppressLint("MissingInflatedId")
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                getLocation()
+            }
+
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                getLocation()
+            }
+
+            else -> {
+                showLocationSettingsDialog()
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -59,8 +76,34 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         mapView.getMapAsync(this)
         return binding.root
     }
+
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
+        requestPermissions()
+    }
+
+    private fun requestPermissions() {
+        if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            && PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            locationPermissionRequest.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        } else {
+            getLocation()
+        }
+    }
+
+    private fun getLocation() {
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -79,25 +122,25 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             val bitmap = BitmapFactory.decodeResource(resources, R.drawable.marker)
             val width = bitmap.width
             val height = bitmap.height
-            val x = width / 2
-            val y = height / 2
+            val x = width / HALF
+            val y = height / HALF
             customMarkerOptions = MarkerOptions()
                 .position(LatLng(it.latitude, it.longitude))
                 .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
                 .anchor(x.toFloat() / width.toFloat(), y.toFloat() / height.toFloat())
-            userMarker = googleMap.addMarker(customMarkerOptions)!!
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userMarker.position, 15f))
+            googleMap?.addMarker(customMarkerOptions)?.let { marker -> userMarker = marker }
+            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(userMarker.position, 15f))
         }
         locationManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER,
-            1000,
-            1f,
+            TIME_UPDATE,
+            DISTANCE_UPDATE,
             locationListener
         )
-        googleMap.isMyLocationEnabled = false
-        googleMap.uiSettings.isMyLocationButtonEnabled = true
+        googleMap?.isMyLocationEnabled = false
+        googleMap?.uiSettings?.isMyLocationButtonEnabled = true
 
-        googleMap.setOnMapClickListener { latLng ->
+        googleMap?.setOnMapClickListener { latLng ->
             addMarkerToMap(latLng)
         }
     }
@@ -105,7 +148,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     private fun addMarkerToMap(latLng: LatLng) {
         val markerOptions = MarkerOptions()
             .position(latLng)
-        googleMap.addMarker(markerOptions)
+        googleMap?.addMarker(markerOptions)
     }
 
     private val locationListener: LocationListener = object : LocationListener {
@@ -115,61 +158,72 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         }
 
         override fun onProviderEnabled(provider: String) {
-            Toast.makeText(context, "Загрузка данных...", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, resources.getText(R.string.txt_loading), Toast.LENGTH_SHORT).show()
+            requestPermissions()
         }
 
         override fun onProviderDisabled(provider: String) {
             showLocationSettingsDialog()
         }
     }
+
     private fun updateLocationUI(location: Location) {
         val latLng = LatLng(location.latitude, location.longitude)
         userMarker.position = latLng
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(latLng))
         customMarkerOptions = MarkerOptions().position(latLng)
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(userMarker.position))
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(userMarker.position))
     }
+
     private fun showLocationSettingsDialog() {
+        dialog?.takeIf { it.isShowing }?.apply { dismiss() }
         val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle("Данные о местоположении отствуют")
-            .setMessage("Пожалуйста разрешите доступ к Вашему местоположению для нормальной работы приложения.")
-            .setPositiveButton("Настройки") { _, _ ->
+        builder.setTitle(resources.getText(R.string.dialog_header_err))
+            .setMessage(resources.getText(R.string.dialog_message_err))
+            .setPositiveButton(resources.getText(R.string.dialog_btn_positive_err)) { _, _ ->
                 val settingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                 val uri = Uri.fromParts("package", requireActivity().packageName, null)
                 settingsIntent.data = uri
                 startActivity(settingsIntent)
             }
-            .setNegativeButton("Отменить") { dialog, _ ->
+            .setNegativeButton(resources.getText(R.string.dialog_btn_negative_err)) { dialog, _ ->
                 dialog.dismiss()
             }
             .create()
             .show()
     }
-    private fun speedDefinition(location: Location){
-        if (lastLocation != null) {
-            val timeDiff = (location.time - lastLocation!!.time) / 1000.0
-            val distance = lastLocation?.distanceTo(location)
-            if(distance != null) speed = distance/timeDiff
+
+    private fun speedDefinition(location: Location) {
+        lastLocation?.let {
+            val timeDiff = (location.time - it.time) / 1000.0
+            val distance = it.distanceTo(location)
+            speed = distance / timeDiff
             lastLocation = location
-            if(speed > 3) {
-                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.Builder()
-                    .target(googleMap.cameraPosition.target)
-                    .zoom(googleMap.cameraPosition.zoom)
-                    .bearing(lastLocation!!.bearing)
-                    .tilt(googleMap.cameraPosition.tilt)
-                    .build()))
-            }else{
+            if (speed > 3) {
+                googleMap?.apply {
+                    moveCamera(
+                        CameraUpdateFactory.newCameraPosition(
+                            CameraPosition.Builder()
+                                .target(cameraPosition.target)
+                                .zoom(cameraPosition.zoom)
+                                .bearing(it.bearing)
+                                .tilt(cameraPosition.tilt)
+                                .build()
+                        )
+                    )
+                }
+            } else {
                 val rotation = location.bearing
-                userMarker.rotation = (rotation-45)
+                userMarker.rotation = (rotation - DEG_ROTATE)
             }
-        }else{
-            lastLocation = location
-        }
+
+        } ?: kotlin.run { lastLocation = location }
     }
 
     override fun onResume() {
         super.onResume()
         mapView.onResume()
+        googleMap?.let { getLocation() }
     }
 
     override fun onPause() {
